22c3dd13d438046736a270ab108d808c4288130a
ERBOL0202 <kul8287@gmail.com>
2025-06-09 22:33:37 +0600

    Update file hw1

---
+++ hw1.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age, city):
+        self.name = name
+        self.age = age
+        self.city = city
+    def introduse(self):
+        print (f"Привет меня зовут {self.name}, мне age {self.age}, мой city {self.city}")
+    def is_adult(self):
+        return self.age >= 18
+person1 = Person("Джинву", 24)
+person2 = Person("ДжинА", 16)
+
+
+

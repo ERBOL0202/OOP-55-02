184d55093f9c38d870c8a438e2b7afc7717631bb
ERBOL0202 <kul8287@gmail.com>
2025-07-03 01:50:43 +0600

    HW:DID hw8

---
+++ lessons/lesson 8.py
@@ -0,0 +1,121 @@
+import sqlite3
+
+connect = sqlite3.connect("users.db")
+cursor = connect.cursor()
+
+def create_table():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users(
+        userid INTEGER PRIMARY KEY AUTOINCREMENT,
+        fio VARCHAR (100) NOT NULL,
+        age INTEGER NOT NULL,
+    )
+''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS grades(
+        gradeid INTEGER PRIMARY KEY AUTOINCREMENT,
+        userid INTEGER, 
+        subject VARCHAR (100) NOT NULL,
+        grade INTEGER NOT NULL,
+        FOREIGN KEY (userid) REFERENCES users(userid)
+    )
+''')
+connect.commit()
+
+def add_user(fio, age):
+    cursor.execute('INSERT INTO users(fio, age) VALUES (?, ?)', (fio, age))
+    connect.commit()
+    print(f"Пользователь {fio} добавлен")
+
+
+# add_user('Илья Муромец', 23)
+# add_user('John Doe1', 24)
+# add_user('John Doe2', 27)
+# add_user('John Doe3', 28)
+# add_user('John Doe4', 35)
+# add_user('John Doe5', 33)
+
+def add_grade(userid, subject, grade):
+    cursor.execute(
+        'INSERT INTO grades(userid, subject, grade) VALUES (?,?,?)',
+        (userid, subject, grade)
+    )
+    print(f"Оценка добавлена для пользователя с ID {userid}")
+    connect.commit()
+
+# add_grade(99, "Алгебре", 5)
+# add_grade(1, "Физика", 3)
+# add_grade(1, "Химия", 4)
+
+def get_users_with_grades():
+    cursor.execute('''
+    SELECT users.fio, grades.subject, grades.grade
+        FROM users FULL OUTER JOIN grades ON users.userid = grades.userid
+    ''')
+
+    rows = cursor.fetchall()
+    for i in rows:
+        print(f"FIO: {i[0]}, SUBJECT: {i[1]}, GRADE: {i[2]}")
+# get_users_with_grades()
+
+
+# AVG() – Среднее значение
+# MAX() – Максимальное значение
+# MIN() – Минимальное значение  COUNT() SUM()
+
+# def get_avg_age():
+#     cursor.execute('SELECT SUM(age) FROM users')
+#     avg_age = cursor.fetchone()
+#
+#     print(avg_age)
+#
+# get_avg_age()
+
+
+
+def create_view_young_users():
+    cursor.execute('''
+        CREATE VIEW IF NOT EXISTS yong_userses AS
+        SELECT fio, age
+        FROM users
+        WHERE age < 25
+    ''')
+   connect.commit()
+   print('Представление young_users создано')
+
+create_view_young_users()
+
+def get_young_users():
+   cursor.execute("""
+        SELECT * FROM yong_userses
+    """)
+
+    users = cursor.fetchall()
+
+    print(users)
+
+get_young_users()
+
+#новое представление
+def create_view_low_grades_chemic():
+    cursor.execute('''
+        CREATE VIEW IF NOT EXISTS grades AS
+        SELECT fio, subject, grade
+        JOIN grades ON user.userid = grades.userid
+        WHERE grade <= 3
+    ''')
+    connect.commit()
+    print('Представление grade создано')
+
+create_view_low_grades_chemic()
+
+def get_low_grades_chemic():
+    cursor.execute("""
+        SELECT * FROM low_grades_chemic
+    """)
+
+    users = cursor.fetchall()
+
+    print(users)
+
+get_low_grades_chemic()_users()
